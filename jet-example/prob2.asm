86dfd6463bb5ffb994704ca89d9f30f2a9a907c1
Jetroid <holtjet@gmail.com>
2016-02-19 11:42:11 +0000

    Fix Jet's Example 2

--- jet-example/prob2.asm
+++ jet-example/prob2.asm
@@ -1,10 +1,10 @@
 ;Copying
-;Take a sequence a1 a2 a3 a4 a5 . . . as an input and output two copies of it
+;Take a sequence a1 a2 a3 a4 a5 . . . as an input and output two copies of it per line
 
 main:
 	MOV r1, stdin		;Read stdin and store in r1
 	CMPNULL r1, @END 	;Jump to end if null
-	MOV stdout, r1		;Output r1 twice
-	MOV stdout, r1
+	MERGE r2, r1, r1	;Make r2 = r1 r1
+	MOV stdout, r2		;Output r2
 	NXT stdin		;Increase stdin to the next value
 	JMP main 		

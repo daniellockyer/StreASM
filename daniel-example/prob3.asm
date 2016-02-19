de8e1c6f482c4ce64197fb5953caf64fff0d8f1c
Daniel Lockyer <thisisdaniellockyer@gmail.com>
2016-02-19 11:18:34 +0000

    Added solution (no error checking) for problem 3

--- daniel-example/prob3.asm
+++ daniel-example/prob3.asm
@@ -3,14 +3,13 @@
 ;	a1 + 3b1 a2 + 3b2 a3 + 3b3 a4 + 3b4 ...
 
 ;Need to do checking for two variables on every input read but worry about that later.
-;TODO - finish this.
-
-READ r1, stdin
-CMPNULL r1, @END
-SPLIT r2, r1, 0 ;Save the first variable in r2
-OUT r2 ;Output the first variable
 
 main:
 	READ r1, stdin
 	CMPNULL r1, @END
+	SPLIT r2, r1, 0 ;Save the first variable in r2
+	SPLIT r3, r1, 1 ;Save the second variable in r3
+	MUL r3, 3 ;Multiply the second by 3
+	ADD r2, r3 ;Add them together
+	OUT r2 ;Output the result
 	JMP main

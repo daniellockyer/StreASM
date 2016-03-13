e48ec21dcd3fa20f3f91e6223c54b3f8d67f5567
Daniel Lockyer <thisisdaniellockyer@gmail.com>
2016-03-13 13:36:43 +0000

    Fixed Problem 1 to execute correctly

--- example/prob1.asm
+++ example/prob1.asm
@@ -2,12 +2,16 @@
 ;Take a sequence a1 a2 a3 a4 a5 ..., as an input and output the sequence 0 a1 a2 a3 ..., that is,
 ;the sequence that is the same as the input sequence, but starting with an additional single 0 character.
 
-	MOV o0, 1
-	MOV o1, 0		;This is the value to prepend
-	NXT stdout, o 	;We act like we did a previous cycle with value 0
+	MOV r1, 0		;This is the value to prepend
 
 main:
-	NXT o, stdin		;Get the input
-	MOV o0, 1
+	NXT i, stdin		;Get the input
+	TSTZ i0, final, @NEXT
+	MOV o1, r1
 	NXT stdout, o
+	MOV r1, i1
 	JMP main		;Loop
+
+final:
+	MOV o1, r1
+	NXT stdout, o

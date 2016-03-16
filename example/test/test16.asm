8fb2d7b6e07199eaa21c3deb2ab7f38ac2de5997
Daniel Lockyer <thisisdaniellockyer@gmail.com>
2016-03-16 11:42:53 +0000

    Renamed our programs to match the spec

---
+++ example/test/test16.asm
@@ -0,0 +1,6 @@
+#DEF reg r0
+
+	MOV r22, 64
+	MOV reg, 22
+	MOV o1, r[reg]
+	NXT stdout, o
--- example/test/test2.asm
+++ example/test/test2.asm
@@ -3,4 +3,4 @@
 	ADD r1, r2, r3 ; r1 = 0, r2 = 0, r3 = 0
 	ADD r4, r1, 4 ; r4 = 4, r1 = 0
 	SUB r3, r[r4], 1 ; r3 = 3, r[r4] = r4 = 4
-	MOV r5, r4 ; r5 = 4, r4 = 4 
+	MOV r5, r4 ; r5 = 4, r4 = 4
